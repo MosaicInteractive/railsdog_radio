1447c9560d1aa6e5dbc9bd1dd3dbd58a9e82b886
Sean Schofield <sean@railsdog.com>
2011-01-02 15:46:05 -0500

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsdogRadio::Application.config.secret_token = '216ee21e7f5668fdde6d9b4b18e01f81a63d48dcfa4f80bba21b162a937ac7b2737e36e917d7b0e4495f78f5b5fcdad9ce6106ec5cd5dee884caf8e60cb99401'

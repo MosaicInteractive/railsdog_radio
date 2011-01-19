8f34a770ec6531e7edfe3174d3e6976dc423f371
Zac Williams <zac@railsdog.com>
2011-01-19 17:42:37 -0600

    completing checkout (still needs some visual clean-up)

--- app/controllers/checkout_controller_decorator.rb
+++ app/controllers/checkout_controller_decorator.rb
@@ -14,13 +14,34 @@ CheckoutController.class_eval do
 
   private
 
+
+  def object_params
+    # For payment step, filter order parameters to produce the expected nested attributes for a single payment and its source, discarding attributes for payment methods other than the one selected
+    if @order.address_and_payment?
+      if params[:payment_source].present? && source_params = params.delete(:payment_source)[params[:order][:payments_attributes].first[:payment_method_id].underscore]
+        params[:order][:payments_attributes].first[:source_attributes] = source_params
+      end
+      if (params[:order][:payments_attributes])
+        params[:order][:payments_attributes].first[:amount] = @order.total
+      end
+      params[:order][:payments_attributes].first[:source_attributes][:first_name] = params[:order][:bill_address_attributes][:firstname]
+      params[:order][:payments_attributes].first[:source_attributes][:last_name] = params[:order][:bill_address_attributes][:lastname]
+      params[:order]
+    end
+  end
+
   def body_id
     @body_id = "checkout"
   end
 
   def before_address_and_payment
-    @order.bill_address ||= Address.new(:country => default_country)
-    @order.ship_address ||= Address.new(:country => default_country)
+    before_address
+    before_payment
+  end
+
+
+  def before_shipping_and_confirm
+    before_delivery
   end
 
 end

8d8d93dcecfef88ace4c296b0a1053d399787889
David North <davidnorth@railsdog.com>
2011-01-26 12:09:05 -0600

    Fix error from nil where product_group expected on homepage

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -2,7 +2,7 @@ class HomepageController < Spree::BaseController
   helper :products
 
   def show
-    @product_groups = Spree::Config[:homepage_groups].split(',').map { |name| ProductGroup.find_by_name(name) }
+    @product_groups = Spree::Config[:homepage_groups].split(',').map { |name| ProductGroup.find_by_name(name) }.compact
   end
 
 end

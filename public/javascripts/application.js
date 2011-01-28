f59f0269cd1ae698407588e2615c79da19a32355
Brian Quinn <briandquinn@gmail.com>
2011-01-28 18:22:38 +0000

    Styling changes

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -53,42 +53,9 @@
       return false;
     });
 
-    //$('#checkout_form_address').validate();
 
-    var get_states = function(region){
-      var country        = $('span#' + region + 'country :only-child').val();
-      return state_mapper[country];
-    }
 
-    var update_state = function(region) {
-      var states         = get_states(region);
-
-      var state_select = $('span#' + region + 'state select');
-      var state_input = $('span#' + region + 'state input');
-
-      if(states) {
-        var selected = state_select.val();
-        state_select.html('');
-        var states_with_blank = [["",""]].concat(states);
-        $.each(states_with_blank, function(pos,id_nm) {
-          var opt = $(document.createElement('option'))
-                    .attr('value', id_nm[0])
-                    .html(id_nm[1]);
-          if(selected==id_nm[0]){
-            opt.attr('selected', 'selected');
-          }
-          state_select.append(opt);
-        });
-        state_select.removeAttr('disabled').show();
-        state_input.hide().attr('disabled', 'disabled');
-
-      } else {
-        state_input.removeAttr('disabled').show();
-        state_select.hide().attr('disabled', 'disabled');
-      }
-
-    };
-
+    //search box
     if(!$.browser.webkit){
       var $input = $('#s1');
       var placeholder = $input.attr('placeholder');

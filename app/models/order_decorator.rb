b2c9601e28b5a20b8e629b737ef614fc60da2bb0
Brian Quinn <briandquinn@gmail.com>
2011-01-31 19:19:03 +0000

    Fixed some stuff... most helpful commit message ever?

--- app/models/order_decorator.rb
+++ app/models/order_decorator.rb
@@ -42,13 +42,16 @@ Order.class_eval do
 
   end
 
-  def create_shipment_with_default!
+  def create_shipment!
     shipping_method ||= available_shipping_methods.first
     save
 
-    create_shipment_without_default!
+    if shipment.present?
+      shipment.update_attributes(:shipping_method => shipping_method)
+    else
+      self.shipments << Shipment.create(:order => self, :shipping_method => shipping_method, :address => self.ship_address)
+    end
   end
 
-  alias_method_chain :create_shipment!, :default
 
 end

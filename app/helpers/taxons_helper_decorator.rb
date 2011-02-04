11905f1860b66e720777e826ae93f26841ba0afc
Brian Quinn <briandquinn@gmail.com>
2011-02-04 13:01:31 +0000

    Breadcrumb tweaks

--- app/helpers/taxons_helper_decorator.rb
+++ app/helpers/taxons_helper_decorator.rb
@@ -1,7 +1,14 @@
 TaxonsHelper.module_eval do
-  def breadcrumbs(taxon, product=nil, separator="&nbsp;&raquo;&nbsp;")
+  def breadcrumbs(options={})
     return "" if current_page?("/")
-    separator = raw(separator)
+
+    options.reverse_merge! :taxon => nil, :product => nil, :separator => "&nbsp;&raquo;&nbsp;"
+
+    taxon         = options[:taxon]
+    product       = options[:product]
+    product_group = options[:product_group]
+    separator     = raw(options[:separator])
+
     crumbs = [content_tag(:li, link_to(t(:home) , root_path) + separator)]
 
     if taxon
@@ -9,10 +16,20 @@ TaxonsHelper.module_eval do
     end
 
     if product
-      crumbs << content_tag(:li, link_to(taxon.name , seo_url(taxon)) + separator)
+      if taxon
+        crumbs << content_tag(:li, link_to(taxon.name, seo_url(taxon)) + separator)
+      elsif product_group
+        crumbs << content_tag(:li, link_to(product_group.name, pg_search_path(:product_group_name => product_group.permalink)) + separator)
+      else
+        crumbs << content_tag(:li, link_to("Products", products_url) + separator)
+      end
       crumbs << content_tag(:li, content_tag(:span, product.name))
-    else
+    elsif taxon
       crumbs << content_tag(:li, content_tag(:span, taxon.name))
+    elsif product_group
+      crumbs << content_tag(:li, content_tag(:span, product_group.name))
+    else
+      crumbs << content_tag(:li, content_tag(:span, "Products"))
     end
     crumb_list = content_tag(:ul, raw(crumbs.flatten.map{|li| li.mb_chars}.join))
     content_tag(:div, crumb_list + tag(:br, {:class => 'clear'}, false, true), :class => 'breadcrumbs')

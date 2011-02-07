cb675eedfaf6420cfb0836b7405dbcb0428714d5
Brian Quinn <briandquinn@gmail.com>
2011-02-07 13:48:28 +0000

    Set active_shipping source location

--- config/initializers/site_preferences.rb
+++ config/initializers/site_preferences.rb
@@ -1,2 +1,7 @@
 Spree::Config.set(:homepage_groups => 'Slingbox,Satellite Radios,Boomboxes,Accessories,Internet Radios,Vehicle Installation')
 Spree::Config.set(:site_name => 'Rails Dog Radio')
+
+Spree::ActiveShipping::Config.set(:origin_country => "US")
+Spree::ActiveShipping::Config.set(:origin_state => "MD")
+Spree::ActiveShipping::Config.set(:origin_city => "Chevy Chase")
+Spree::ActiveShipping::Config.set(:origin_zip => "20815")

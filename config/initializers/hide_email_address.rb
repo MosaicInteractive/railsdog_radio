2e79fd7a81ab4f21eb0d076ba51dcdf8ede54202
Sean Schofield <sean@railsdog.com>
2011-03-16 14:27:14 -0400

    Revert "replace email address with asterik"  We're not giving the general public access to the admin so there is no need for this feature.
    This reverts commit 8c2f97be1fe7d0c5d4f7df38acdcc50835ac0845.

--- config/initializers/hide_email_address.rb
+++
@@ -1,7 +0,0 @@
-class SpreeStaticContentHooks < Spree::ThemeSupport::HookListener
-
-  replace :admin_users_index_rows do
-    %Q{ <td width="350px"><%=link_to user.email.gsub(/./,'*'), object_url(user) %></td> }
-  end
-
-end
